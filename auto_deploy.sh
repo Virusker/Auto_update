48b895a766f93c447d914f7e824f750379b12986
BuildTools <unconfigured@null.spigotmc.org>
2024-02-23 21:19:46 +0000

    up

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo $2
 cd $2
-git config --global --add safe.directory $2
-git pull  # hoặc branch bạn đang sử dụng
+/usr/bin/git config --global --add safe.directory $2
+/usr/bin/git pull  # hoặc branch bạn đang sử dụng
 #systemctl restart your_app.service  # khởi động lại ứng dụng
 sudo supervisorctl restart $1
 
