e03c9c763f555d3682ba92e8b3ef1f0dac61f67f
BuildTools <unconfigured@null.spigotmc.org>
2024-02-23 17:46:59 +0000

    update

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 cd app/test/
 git pull  # hoặc branch bạn đang sử dụng
 #systemctl restart your_app.service  # khởi động lại ứng dụng
-sudo supervisorctl restart app
+sudo supervisorctl restart $1
 
-sudo supervisorctl status app
+sudo supervisorctl status $1
