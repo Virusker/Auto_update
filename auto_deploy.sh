892cb6cea7719d46354f3fd56e8e34b0e8e7286e
kh4n <phanthekhanh14@gmail.com>
2024-02-25 00:01:14 +0700

    up

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -4,6 +4,5 @@ cd $2
 /usr/bin/git config --global --add safe.directory $2
 /usr/bin/git pull  # hoặc branch bạn đang sử dụng
 #systemctl restart your_app.service  # khởi động lại ứng dụng
-sudo supervisorctl restart $1
 
-sudo supervisorctl status $1
+systemctl restart $1
