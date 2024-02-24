bc15daee19ea66e3c3acab60a680a1239f011760
kh4n <phanthekhanh14@gmail.com>
2024-02-25 02:17:13 +0700

    up

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -7,4 +7,4 @@ export HOME=/home
 /usr/bin/git pull  # hoặc branch bạn đang sử dụng
 #systemctl restart your_app.service  # khởi động lại ứng dụng
 
-sudo /usr/bin/systemctl restart $1
+/usr/bin/systemctl restart $1
